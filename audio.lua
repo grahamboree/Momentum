06fcae5afcf60c6b66837191f9686645eb7bf639
Graham Pentheny <graham.pentheny@gmail.com>
2018-01-28 13:50:54 -0500

    Additional sounds

--- audio.lua
+++ audio.lua
@@ -20,6 +20,20 @@ local function load()
 	sounds.sfx["positiveinteract"] = love.audio.newSource("SFX/positiveinteract.wav", "static")
 	sounds.sfx["selectionclick1"] = love.audio.newSource("SFX/selectionclick1.wav", "static")
 	sounds.sfx["selectionclick2"] = love.audio.newSource("SFX/selectionclick2.wav", "static")
+
+	sounds.sfx["objectsound1"] = love.audio.newSource("SFX/objectsound1_loop.wav", "static")
+	sounds.sfx["objectsound2"] = love.audio.newSource("SFX/objectsound2_loop.wav", "static")
+	sounds.sfx["objectsound3"] = love.audio.newSource("SFX/objectsound3_loop.wav", "static")
+	sounds.sfx["objectsound4"] = love.audio.newSource("SFX/objectsound4_loop.wav", "static")
+	sounds.sfx["objectsound5"] = love.audio.newSource("SFX/objectsound5_loop.wav", "static")
+	sounds.sfx["objectsound6"] = love.audio.newSource("SFX/objectsound6_loop.wav", "static")
+
+	sounds.sfx.objectsound1:isLooping(true)
+	sounds.sfx.objectsound2:isLooping(true)
+	sounds.sfx.objectsound3:isLooping(true)
+	sounds.sfx.objectsound4:isLooping(true)
+	sounds.sfx.objectsound5:isLooping(true)
+	sounds.sfx.objectsound6:isLooping(true)
 end
 
 return {
