6efc7a950c2450f6ee14db4bcc05ea3f68145416
Graham Pentheny <graham.pentheny@gmail.com>
2018-01-27 19:50:55 -0500

    Rect obstacles

--- Elements/elementRectangle.lua
+++ Elements/elementRectangle.lua
@@ -36,4 +36,50 @@ function ElementRectangle:setDefaults()
   if not self.color then self.color = {r = 0, g = 0, b = 255} end
 end
 
-    
+
+function ElementRectangle:modifyBoid(i, boidData, addIfPossible)
+  self:setDefaults()
+
+  local boidX = boidData.positions[i][1]
+  local boidY = boidData.positions[i][2]
+
+  if self:pointIsInside(boidX, boidY) then 
+
+
+    local xMin = self.x - self.width / 2
+    local xMax = self.x + self.width / 2
+    local yMin = self.y - self.height / 2
+    local yMax = self.y + self.height / 2
+
+    local xMinDelta = math.abs(xMin - boidX)
+    local yMinDelta = math.abs(yMin - boidY)
+    local xMaxDelta = math.abs(xMax - boidX)
+    local yMaxDelta = math.abs(yMax - boidY)
+
+    local boidVX = boidData.velocities[i][1]
+    local boidVY = boidData.velocities[i][2]
+
+    if xMinDelta < yMinDelta and xMinDelta < xMaxDelta and xMinDelta < yMaxDelta then
+      boidX = xMin
+      boidVX = -boidVX
+    elseif yMinDelta < xMinDelta and yMinDelta < xMaxDelta and yMinDelta < yMaxDelta then
+      boidY = yMin
+      boidVY = -boidVY
+    elseif xMaxDelta < yMinDelta and xMaxDelta < xMinDelta and xMaxDelta < yMaxDelta then
+      boidX = xMax
+      boidVX = -boidVX
+    else
+      --yMaxDelta
+      boidY = yMax
+      boidVY = -boidVY
+    end
+
+    boidData.positions[i][1] = boidX
+    boidData.positions[i][2] = boidY
+
+    boidData.velocities[i][1] = boidVX
+    boidData.velocities[i][2] = boidVY
+  end
+end
+
+
